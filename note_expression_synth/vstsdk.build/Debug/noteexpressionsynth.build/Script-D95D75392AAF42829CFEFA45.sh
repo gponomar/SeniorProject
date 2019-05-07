bfc6ba54fd1f3875e834d7eef22b2479ddb5e93b
gponomar <gponomar@calpoly.edu>
2019-05-07 14:33:34 -0700

    added filters

---
+++ note_expression_synth/vstsdk.build/Debug/noteexpressionsynth.build/Script-D95D75392AAF42829CFEFA45.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/GracieP/Desktop/School/2018-2019/Senior\ Project/vst3sdk/build/public.sdk/samples/vst/note_expression_synth -f /Users/GracieP/Desktop/School/2018-2019/Senior\ Project/vst3sdk/build/public.sdk/samples/vst/note_expression_synth/CMakeScripts/noteexpressionsynth_postBuildPhase.make$CONFIGURATION all
